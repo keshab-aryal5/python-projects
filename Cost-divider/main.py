eeb45e46974fc1b0fc66fd20e500173c917c50ee
keshab Aryal <keshabaryal152@gmail.com>
2024-06-08 10:41:14 +0545

    fixed bug and handled exception

--- Cost-divider/main.py
+++ Cost-divider/main.py
@@ -19,58 +19,61 @@ for x in range(1,total_member+1):
     total_cost += contribution
     print("\n")
 
-average = round(total_cost/total_member,2)
+try:
+    average = round(total_cost/total_member,2)
 
-print(f"Total money spent is Rs.{total_cost} and each member should pay Rs. {average}")
+except ZeroDivisionError:
+    print("0 people 0 cost.\nBye Bye")
 
-# categorizing the members as more and less payers based on their contribution
-for x in members:
-    x.calculate(average)
-    if x.status == 1:
-        more_payers.append(x)
-    else:
-        less_payers.append(x)
+else:    
+    print(f"Total money spent is Rs.{total_cost} and each member should pay Rs. {average}")
 
-# sorting the members according to the highest amount to receive or pay
-more_payers.sort(key=lambda x: x.payment,reverse=True)
-less_payers.sort(key=lambda x: x.payment,reverse=True)
+    # categorizing the members as more and less payers based on their contribution
+    for x in members:
+        x.calculate(average)
+        if x.status == 1:
+            more_payers.append(x)
+        else:
+            less_payers.append(x)
 
-for x in more_payers:
-    print(x.name)
+    # sorting the members according to the highest amount to receive or pay
+    more_payers.sort(key=lambda x: x.payment,reverse=True)
+    less_payers.sort(key=lambda x: x.payment,reverse=True)
 
-for x in less_payers:
-    print(x.name)
 
-# final logic for calculation of payment details from less payers to more payers
-for less in less_payers:
-    while less.payment > 0:
-        for more in more_payers:
-            if more.payment == 0:
-                more_payers.remove(more)
-                continue
-            elif less.payment > more.payment:
-                if less.name in paying_details:
-                    paying_details[less.name][more.name] = more.payment
+    # final logic for calculation of payment details from less payers to more payers
+    status = True
+    for less in less_payers:
+        while less.payment > 0 and status:
+            for more in more_payers:
+                if more.payment == 0:
+                    more_payers.remove(more)
+                    if len(more_payers) == 0:
+                        status = False
+                    continue
+                elif less.payment > more.payment:
+                    if less.name in paying_details:
+                        paying_details[less.name][more.name] = more.payment
+                    else:
+                        new_dict = {more.name : more.payment}
+                        paying_details[less.name] = new_dict
+
+                    less.payment = round(less.payment - more.payment,2)
+                    more.payment = 0
                 else:
-                    new_dict = {more.name : more.payment}
-                    paying_details[less.name] = new_dict
+                    if less.name in paying_details:
+                        paying_details[less.name][more.name] = less.payment
+                    else:
+                        new_dict = {more.name : less.payment}
+                        paying_details[less.name] = new_dict
 
-                less.payment = less.payment - more.payment
-                more.payment = 0
-            else:
-                if less.name in paying_details:
-                    paying_details[less.name][more.name] = less.payment
-                else:
-                    new_dict = {more.name : less.payment}
-                    paying_details[less.name] = new_dict
+                    more.payment = round(more.payment - less.payment,2)
+                    less.payment = 0
+                    break
 
-                more.payment = more.payment - less.payment
-                less.payment = 0
-                break
-
-for x in paying_details:
-    print(f"{x} should pay: ")
-    for y in paying_details[x]:
-        print("    ",end=" ")
-        print(f"Rs {paying_details[x][y]} to {y}")
-    print("\n")
+    for x in paying_details:
+        print(f"{x} should pay: ")
+        for y in paying_details[x]:
+            print("    ",end=" ")
+            print(f"Rs {paying_details[x][y]} to {y}")
+        print("\n")
