ca271bf29f9c7dd2389be1e42c6d0aa98c3bf152
keshab Aryal <keshabaryal152@gmail.com>
2024-06-09 22:41:46 +0545

    fixed another bug in the program

--- Cost-divider/member.py
+++ Cost-divider/member.py
@@ -1,7 +1,27 @@
+class InvalidContribution(Exception):
+    '''Catches if negative contribution is given.
+    parameter messge -- Messge to display if negative contribution is done.
+    '''
+    def __init__(self,message="Contribution cann't be negative."):
+        self.message = message
+        super().__init__(self.message)
+        
+
+
 class Member:
     def __init__(self, name, contribution):
         self.name = name
-        self.contribution = contribution
+        try:
+            if contribution < 0:
+                raise InvalidContribution
+
+        except InvalidContribution as e:
+            print(e)
+            exit(1)
+        
+        else:
+            self.contribution = contribution
+        
         self.status = 0
         self.payment = 0
 
